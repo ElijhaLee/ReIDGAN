e47afffdd43dcee3fab3d74d4c42e1ef7da67f91
elijha <elijhalee1994@gmail.com>
2017-10-24 17:27:44 +0800

    Initial commit

---
+++ model/discriminator.py
@@ -0,0 +1,28 @@
+class Discriminator(nn.Module):
+    def __init__(self):
+        super(Discriminator, self).__init__()
+
+        main = nn.Sequential(
+            nn.Conv2d(1, DIM, 5, stride=2, padding=2),
+            # nn.Linear(OUTPUT_DIM, 4*4*4*DIM),
+            nn.ReLU(True),
+            nn.Conv2d(DIM, 2*DIM, 5, stride=2, padding=2),
+            # nn.Linear(4*4*4*DIM, 4*4*4*DIM),
+            nn.ReLU(True),
+            nn.Conv2d(2*DIM, 4*DIM, 5, stride=2, padding=2),
+            # nn.Linear(4*4*4*DIM, 4*4*4*DIM),
+            nn.ReLU(True),
+            # nn.Linear(4*4*4*DIM, 4*4*4*DIM),
+            # nn.LeakyReLU(True),
+            # nn.Linear(4*4*4*DIM, 4*4*4*DIM),
+            # nn.LeakyReLU(True),
+        )
+        self.main = main
+        self.output = nn.Linear(4*4*4*DIM, 1)
+
+    def forward(self, input):
+        input = input.view(-1, 1, 28, 28)
+        out = self.main(input)
+        out = out.view(-1, 4*4*4*DIM)
+        out = self.output(out)
+        return out.view(-1)
---
+++ model/selector.py
@@ -0,0 +1,64 @@
+import torch
+import torch.autograd as autograd
+import torch.nn as nn
+import torch.functional as F
+
+IN_DIM = 1024
+BATCH_SIZE = 32
+
+
+class Selector(nn.Module):
+    def __init__(self):
+        super(Selector, self).__init__()
+
+        self.linear0 = nn.Linear(IN_DIM, 2 * IN_DIM)
+        self.relu0 = nn.ReLU(True)
+
+        self.linear1 = nn.Linear(2 * IN_DIM, 4 * IN_DIM)
+        self.bn1 = nn.BatchNorm1d(4 * IN_DIM)
+        self.relu1 = nn.ReLU(inplace=True)
+
+        self.linear2 = nn.Linear(4 * IN_DIM, 8 * IN_DIM)
+        self.bn2 = nn.BatchNorm1d(8 * IN_DIM)
+        self.relu2 = nn.ReLU(inplace=True)
+
+        self.linear3 = nn.Linear(8 * IN_DIM, BATCH_SIZE)
+
+    def forward(self, input):
+        x = self.linear0(input)
+        x = self.relu0(x)
+
+        x = self.linear1(x)
+        x = self.bn1(x)
+        x = self.relu1(x)
+
+        x = self.linear2(x)
+        x = self.bn2(x)
+        x = self.relu2(x)
+
+        x = self.linear3(x)
+
+        y = nn.Softmax()(x)
+
+        # output = output.view(-1, 4 * DIM, 4, 4)
+        # # print output.size()
+        # output = self.block1(output)
+        # # print output.size()
+        # output = output[:, :, :7, :7]
+        # # print output.size()
+        # output = self.block2(output)
+        # # print output.size()
+        # output = self.deconv_out(output)
+        # output = self.sigmoid(output)
+        # # print output.size()
+        # return output.view(-1, OUTPUT_DIM)
+        return x, y
+
+
+if __name__ == '__main__':
+    s = Selector()
+    input_ = torch.autograd.Variable(torch.Tensor(BATCH_SIZE, 1024))
+    o1, o2 = s(input_)
+    nn.Parameter
+    nn.Conv2d
+    print()
